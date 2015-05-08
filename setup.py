96ab07162f7b652907093dee564b4a43bd7f0453
ghisprince <ghisprince@gmail.com>
2015-05-08 09:40:57 -0700

    simplified

--- setup.py
+++ setup.py
@@ -4,30 +4,6 @@ import os
 import glob
 import shutil
 
-class BuildToolboxSupportFiles(setuptools.command.build_py.build_py):
-    def run(self):
-        import arcpy
-        setuptools.command.build_py.build_py.run(self)
-
-        esri_pth = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                                                      "build",
-                                                      "lib",                                
-                                                      "esri")
-        tbx_pth = os.path.join(esri_pth, "toolboxes")
-
-        # find all tbx & pyt, create arcpy & help files for them
-        for f in (glob.glob(os.path.join(tbx_pth, "*.pyt")) +
-                  glob.glob(os.path.join(tbx_pth, "*.tbx"))):
-            arcpy.gp.CreateToolboxSupportFiles(f)
-
-        for root, dirs, files in os.walk(tbx_pth + "\\esri", topdown=False):
-            for f in files:
-                import pdb;pdb.set_trace()
-                
-                os.renames(os.path.join(root, f),
-                      os.path.join(root.replace(tbx_pth + "\\esri", esri_pth), f))
-
-        
 
 setup(name             = "xtragptools",
       version          = 0.1,
@@ -40,9 +16,11 @@ setup(name             = "xtragptools",
       package_dir      = {"": "src"},
       packages         = ["",],
       package_data     = {"": ["esri/toolboxes/*.*",
-                               "esri/help/gp/messages/*.xml",                               
-                                       ] },
-      cmdclass         = {'build_py': BuildToolboxSupportFiles},
+                               "esri/help/gp/toolboxes/*.xml",
+                               "esri/help/gp/messages/*.xml",
+                               "esri/help/gp/*.xml",
+                               "esri/arcpy/*.*",
+                              ] },
       classifiers      = [
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
